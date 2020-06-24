edcb5097e4ff144593785b8a343b35c6bff65251
c4rlosc7 <camartinez7@hotmail.com>
2020-06-24 08:36:39 -0500

    fixed pojo with lombok lib

--- CaesarCipher/caesar-cipher-app/src/main/java/com/carlos/app/caesar/domain/model/CaesarCipher.java
+++ CaesarCipher/caesar-cipher-app/src/main/java/com/carlos/app/caesar/domain/model/CaesarCipher.java
@@ -1,52 +1,22 @@
 package com.carlos.app.caesar.domain.model;
 
 import com.carlos.app.caesar.domain.caesar.CipherCaesarImpl;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.util.Arrays;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class CaesarCipher extends CipherCaesarImpl {
     public String plainText;
     public char[] cipherText;
     public int key;
 
-    public CaesarCipher() {
-    }
-
     public CaesarCipher(String plainText, int key) {
         this.plainText = plainText;
         this.key = key;
-    }
-
-    public String getPlainText() {
-        return plainText;
-    }
-
-    public void setPlainText(String plainText) {
-        this.plainText = plainText;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public char[] getCipherText() {
-        return cipherText;
-    }
-
-    public void setCipherText(char[] cipherText) {
-        this.cipherText = cipherText;
-    }
-
-    @Override
-    public String toString() {
-        return "CaesarCipher{" +
-                "plainText='" + plainText + '\'' +
-                ", cipherText=" + Arrays.toString(cipherText) +
-                ", key=" + key +
-                '}';
     }
 }
