f5240f242225fe98c3254cb19235ae76ce98cc3f
c4rlosc7 <camartinez7@hotmail.com>
2020-06-24 08:25:17 -0500

    add a new item caesar cipher

--- CaesarCipher/demo-caesar/src/main/java/com/carlos/app/caesar/domain/Solution.java
+++ CaesarCipher/demo-caesar/src/main/java/com/carlos/app/caesar/domain/Solution.java
@@ -4,7 +4,10 @@ import com.carlos.app.caesar.domain.model.CaesarCipher;
 
 public class Solution {
     public static void main(String[] args) {
-        CaesarCipher caesar = new CaesarCipher("Hello", 3);
+        // Plaintext:  THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+        // Ciphertext: QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD
+        // Key : 3
+        CaesarCipher caesar = new CaesarCipher("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 3);
         caesar.cipher(caesar);
         System.out.println("PlanText: " + caesar.getPlainText());
         System.out.println("CipherText: " + new String(caesar.getCipherText()));
